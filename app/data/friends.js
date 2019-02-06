8ee25dd2b6fbabbe9c30905f19a0197c23886b88
Stacy Acker <stacyacker@Stacys-MacBook-Pro.local>
2019-02-05 19:20:35 -0500

    final pics fixed

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
 	{
 		name: 'Baker Mayfield',
-		photo: '.../images/baker.jpg',
+		photo: 'https://ewscripps.brightspotcdn.com/dims4/default/de8bca9/2147483647/strip/true/crop/640x360+0+34/resize/1280x720!/quality/90/?url=https%3A%2F%2Fmediaassets.news5cleveland.com%2Fphoto%2F2018%2F11%2F25%2FGettyImages-1065208794_1543182915576_104065339_ver1.0_640_480.jpg',
 		scores: [1, 5, 1, 4, 4, 1, 1, 5, 1, 5]
 	},{
 		name: 'Alex Morgan',
@@ -9,11 +9,11 @@ var friends = [
 		scores: [4, 1, 4, 5, 1, 1, 3, 5, 4, 1]
 	},{
 		name: 'Will Farrell',
-		photo: '../images/will farrell.jpg',
+		photo: 'http://www2.pictures.zimbio.com/gi/Will+Ferrell+First+Annual+Comedy+Awards+Arrivals+sguITWTeCBql.jpg',
 		scores: [2, 1, 2, 3, 5, 5, 1, 3, 5, 4]
 	},{
 		name: 'Kermit',
-		photo: '../images/kermit.jpeg',
+		photo: 'https://en.wikipedia.org/wiki/Kermit_the_Frog#/media/File:Kermit_the_Frog.jpg',
 		scores: [5, 1, 5, 1, 4, 1, 5, 1, 3, 1]
 	}];
 
